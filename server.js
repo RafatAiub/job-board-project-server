3300bb67a33d956863360fd1f1bef871f95e2257
RafatAiub <tanvirmahtab1996@gmail.com>
2025-07-23 12:28:48 +0600

    fix: remove node_modules from tracking and apply .gitignore

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+const jobRoutes = require('./routes/jobRoutes');
+const applicationRoutes = require('./routes/applicationRoutes');
+const connectDB = require('./config/db');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Connect to DB
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/jobs', jobRoutes);
+app.use('/api/applications', applicationRoutes);
+
+// Base route
+app.get('/', (req, res) => {
+  res.send('Job Board API is running...');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
